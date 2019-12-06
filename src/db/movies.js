3fbf30222557297339dcdf6fdd5b0ee6fa33ad9b
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2019-12-06 13:30:20 -0600

    Refactor populate-db and create separate files for setting up movies and users db data.
    The file populate-db.js should just be used to populate the db. I'm
    breaking out the logic that pulls in the movie data from The Movie
    Database API into it's own file. I also created a file for the users
    db collection, which will use faker to create some user accounts.

---
+++ src/db/movies.js
@@ -0,0 +1,13 @@
+const bent = require('bent');
+const mongoose = require('mongoose');
+const Movie = require('../models/movie');
+
+const getJSON = bent('json');
+
+exports.getMovieData = async () => {
+  const movieData = await getJSON('https://api.themoviedb.org/3/movie/popular?api_key=b0f31f3b68b27703024d00cce1fa4366&language=en-US&page=1');
+  return movieData.results.map((movie) => {
+    const { title, overview, popularity } = movie;
+    return new Movie({ title, overview, popularity });
+  });
+};
--- src/db/populate-db.js
+++ src/db/populate-db.js
@@ -1,16 +1,5 @@
-const bent = require('bent');
 const mongoose = require('mongoose');
-const Movie = require('../models/movie');
-
-const getJSON = bent('json');
-
-const getMovieData = async () => {
-  const movieData = await getJSON('https://api.themoviedb.org/3/movie/popular?api_key=b0f31f3b68b27703024d00cce1fa4366&language=en-US&page=1');
-  return movieData.results.map((movie) => {
-    const { title, overview, popularity } = movie;
-    return new Movie({ title, overview, popularity });
-  });
-};
+const movies = require('./movies');
 
 const connectionOptions = {
   useNewUrlParser: true,
@@ -30,7 +19,7 @@ mongoose.connection.on('connected', async () => {
   console.log('Connected successfully to DB!');
   
   try {
-    const movieData = await getMovieData();
+    const movieData = await movies.getMovieData();
     movieData.forEach(async (movie) => await movie.save());
   } catch (e) {
     console.log(e);
