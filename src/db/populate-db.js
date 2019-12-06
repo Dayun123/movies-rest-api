0d83a397a688ea9b5632b635da76004e9619b837
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2019-12-06 12:02:10 -0600

    Get and format some movie data from The Movie Database API.

--- src/db/populate-db.js
+++ src/db/populate-db.js
@@ -1,8 +1,19 @@
-const bent = require('bent')
+const bent = require('bent');
 
-const getJSON = bent('json')
+const getJSON = bent('json');
+
+const getMovieData = async () => {
+  const movieData = await getJSON('https://api.themoviedb.org/3/movie/popular?api_key=b0f31f3b68b27703024d00cce1fa4366&language=en-US&page=1');
+  return movieData.results.map((movie) => {
+    const { title, overview, popularity } = movie;
+    return { title, overview, popularity };
+  });
+};
 
 (async () => {
-  const movieData = await getJSON('https://api.themoviedb.org/3/movie/popular?api_key=b0f31f3b68b27703024d00cce1fa4366&language=en-US&page=1');
+  const movieData = await getMovieData();
   console.log(movieData);
 })();
+
+
+
