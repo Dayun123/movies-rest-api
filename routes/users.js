97058638c48de16008766fbad18397e254b03cc5
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2019-12-09 17:29:09 -0600

    Enforce correct 'Content-Type' header at POST /users route.
    I expect the 'Content-Type' header for POST /users to be 'application/json',
    as this route is used to create a new user from the JSON object sent
    in the request body.

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,12 @@ const express = require('express');
 const router = express.Router();
 
 router.post('/', (req, res, next) => {
+  if (req.get('Content-Type') === 'application/json') {
+    return res.json({
+      statusCode: 400,
+      statusMessage: 'Requests to POST /users must have the following header: `Content-Type: application/json`',
+    });
+  }
   res.json(req.body);
 });
 
