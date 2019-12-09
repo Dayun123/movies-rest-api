94a0880b4e668b6d2536cb40f48b7f909a1dfb12
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2019-12-09 17:02:49 -0600

    Update src/db/movies.js.
    I'm tracking the release date of movies now instead of the
    popularity, so needed to change the information pulled from the
    HTTP request to The Movie Database API.

--- src/db/movies.js
+++ src/db/movies.js
@@ -7,7 +7,7 @@ const getJSON = bent('json');
 exports.createMovieData = async () => {
   const movieData = await getJSON('https://api.themoviedb.org/3/movie/popular?api_key=b0f31f3b68b27703024d00cce1fa4366&language=en-US&page=1');
   return movieData.results.map((movie) => {
-    const { title, overview, popularity } = movie;
-    return new Movie({ title, overview, popularity });
+    const { title, overview, release_date } = movie;
+    return new Movie({ title, overview, releaseDate: release_date });
   });
 };
