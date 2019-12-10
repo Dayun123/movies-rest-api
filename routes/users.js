d39d8439f24c7d9204344647009f1e011e778738
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2019-12-09 21:02:24 -0600

    Return correct status code for invalid Content-Type header at POST /users.

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,7 @@ router.post('/', async (req, res, next) => {
   
   // HTTP validation seems logical to include in the route-handler, while resource-level validation is handled in the db.js file
   if (req.get('Content-Type') !== 'application/json') {
-    return res.json({
+    return res.status(400).json({
       statusCode: 400,
       statusMessage: 'Requests to POST /users must have the following header: `Content-Type: application/json`',
     });
