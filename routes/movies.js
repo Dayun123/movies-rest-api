6d98ee9ae3268d3f79d7cd5c5d645dd5f85874a1
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2019-12-10 22:46:32 -0600

    Add router-level middleware in movies router to check for apiKey param in query string.
    This middleware just checks that the apiKey parameter appears in
    the query string, not if it is a correct apiKey.

--- routes/movies.js
+++ routes/movies.js
@@ -3,6 +3,17 @@ const db = require('../src/db/db');
 
 const router = express.Router();
 
+router.use((req, res, next) => {
+  if (!req.query.apiKey) {
+    res.status(400).json({
+      statusCode: 400,
+      statusMessage: `Access to ${req.method} ${req.baseUrl} requires an apiKey in the query string`,
+    });
+  } else {
+    next();
+  }
+});
+
 router.get('/', (req, res, next) => {
   res.json([{
     msg: "Success",
