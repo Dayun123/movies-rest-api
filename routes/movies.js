50531cce96995b347d5fde8751df82479f391fde
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2019-12-10 23:15:13 -0600

    Refactor movie router apiKey validation middleware to be in one function.
    I originally had two router.use() middleware calls, but consolidated
    them into one.

--- routes/movies.js
+++ routes/movies.js
@@ -3,27 +3,26 @@ const db = require('../src/db/db');
 
 const router = express.Router();
 
-router.use((req, res, next) => {
+router.use(async (req, res, next) => {
+  
   if (!req.query.apiKey) {
-    res.status(400).json({
+    return res.status(400).json({
       statusCode: 400,
       statusMessage: `Access to ${req.method} ${req.baseUrl} requires an apiKey in the query string`,
     });
-  } else {
-    next();
   }
-});
 
-router.use(async (req, res, next) => {
   const isValidApiKey = await db.validateApiKey(req.query.apiKey);
+
   if (!isValidApiKey) {
-    res.status(401).json({
+    return res.status(401).json({
       statusCode: 401,
       statusMessage: 'Must provide a valid API Key',
     });
-  } else {
-    next();
-  }
+  } 
+
+  next();
+  
 });
 
 router.get('/', async (req, res, next) => {
