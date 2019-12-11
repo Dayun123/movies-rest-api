7e0b1ae146b41553c71cdfec399d675cc33ea680
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2019-12-10 21:46:29 -0600

    Remove excess files and folders from app that were auto-generated by express-generator.

--- app.js
+++ app.js
@@ -9,10 +9,7 @@ const app = express();
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 app.use((err, req, res, next) => {
