f49f9af1272388af565589b7a71fbc15da30b4c6
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2019-12-10 20:53:33 -0600

    Setup db connection in flush-db.js.

---
+++ src/db/flush-db.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const movies = require('./movies');
+const users = require('./users');
+
+const connectionOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+mongoose.connect('mongodb://localhost:27017/moviesRestAPI', connectionOptions)
+  .catch((error) => {
+    console.log('Connection error: ', error);
+  });
+
+mongoose.connection.on('error', (error) => {
+  console.log(error);
+});
+
+mongoose.connection.on('connected', () => {
+  console.log('Connected to DB');
+});
