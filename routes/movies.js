f3456858288bc81a2e88295052130725f399dc39
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2019-12-12 11:54:29 -0600

    Refactor middelware in movies router to use separate module to validate existence of api key.

--- routes/movies.js
+++ routes/movies.js
@@ -1,16 +1,12 @@
 const express = require('express');
 const db = require('../src/db/db');
+const validate = require('./validate');
 
 const router = express.Router();
 
+router.use(validate.apiKeyExistsInQS);
+
 router.use(async (req, res, next) => {
-  
-  if (!req.query.apiKey) {
-    return res.status(400).json({
-      statusCode: 400,
-      statusMessage: `Access to ${req.method} ${req.baseUrl} requires an apiKey in the query string`,
-    });
-  }
 
   try {
    
