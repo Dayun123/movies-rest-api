0fbef8004db6b93f16b00ca7a3580bdebacf4392
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2019-12-16 19:44:14 -0600

    Refactor POST /users router to use validate.js for content-type checking.

--- routes/users.js
+++ routes/users.js
@@ -5,25 +5,15 @@ const utils = require('../src/utils');
 
 const router = express.Router();
 
+router.post('/', validate.contentTypeJSON);
+
 router.post('/', async (req, res, next) => {
-  
-  // HTTP validation seems logical to include in the route-handler, while resource-level validation is handled in the db.js file
-  if (req.get('Content-Type') !== 'application/json') {
-    return res.status(400).json({
-      statusCode: 400,
-      statusMessage: 'Requests to POST /users must have the following header: `Content-Type: application/json`',
-    });
-  }
-  
   try {
-    // this could be a successful response which includes the created user, or some sort of error response. If something goes wrong at the db connection level, an the promise will reject and the catch block will run, but otherwise the db.create() method will return a response we can use.
     const dbResponse = await db.create('user', req.body);
     res.status(dbResponse.statusCode).json(dbResponse);
   } catch (e) {
-    // pass along the error to the app-level error-handling middleware, we expect it to be some sort of db connection issue.
     next(e);
   }
-
 });
 
 router.use(validate.apiKeyExistsInQS);
