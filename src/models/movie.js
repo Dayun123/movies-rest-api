8b5b126bc963d5a126e7f41fe8d3af8ecf56e60a
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2019-12-16 19:52:31 -0600

    Require all fields in Movie schema.

--- src/models/movie.js
+++ src/models/movie.js
@@ -3,11 +3,14 @@ const mongoose = require('mongoose');
 module.exports = mongoose.model('Movie', new mongoose.Schema({
   title: {
     type: String,
+    required: true,
   },
   overview: {
     type: String,
+    required: true,
   },
   releaseDate: {
     type: Date,
+    required: true,
   },
 }));
